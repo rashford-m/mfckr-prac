c86924fd6a7d5e71167b2c3b79bafb50aca92afc
Rashford <mikearowo@gmail.com>
2023-05-31 11:02:17 +0100

    updated

---
+++ contracts/constr.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    uint256 public myNumber;
+
+    constructor(uint256 _initialNumber) {
+        myNumber = _initialNumber;
+    }
+}
